194a67b385b7c38af2b2c3acab170f0c60f689b8
LaimaLP <pileliene.laima@gmail.com>
2024-01-10 00:50:41 +0200

    break removed

--- classes/Calc.class.php
+++ classes/Calc.class.php
@@ -1,39 +1,40 @@
 <?php
 
-class Calc{
+class Calc
+{
     public $operator;
     public $num1;
     public $num2;
 
 
-    public function __construct(string $one, int $two, int $three){
+    public function __construct(string $one, int $two, int $three)
+    {
         $this->operator = $one;
         $this->num1 = $two;
         $this->num2 = $three;
     }
 
-    public function calculator(){   
-        switch($this->operator){
+    public function calculator()
+    {
+        switch ($this->operator) {
             case 'add':
-             $result = $this->num1 + $this->num2;
-             return $result;
-             break;
+             
+                return $this->num1 + $this->num2;
             case 'sub':
-             $result = $this->num1 - $this->num2;
-             return $result;
-             break;
-             case 'mul':
-            $result = $this->num1 * $this->num2;
-            return $result;
-            break;
+                $result = $this->num1 - $this->num2;
+                return $result;
+
+            case 'mul':
+                $result = $this->num1 * $this->num2;
+                return $result;
+
             case 'div':
-            $result = $this->num1 / $this->num2;
-            return $result;
-            break;
+                $result = $this->num1 / $this->num2;
+                return $result;
+
             default:
-            echo "Error!";
-            break;
+                echo "Error!";
+                break;
         }
     }
-
 }
